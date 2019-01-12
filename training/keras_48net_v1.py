099859b136c56f8e527acff53cc7477aa259279a
wkkunique <wkkunique@gmail.com>
2019-01-12 13:41:18 +0800

    using pytables

--- training/keras_48net_v1.py
+++ training/keras_48net_v1.py
@@ -16,18 +16,18 @@ import keras
 import os
 import msgpack
 import msgpack_numpy as m
+import tables
 
 import sys
 sys.path.append('/home/wk/e/mtcnn/keras-mtcnn/')
 from MTCNNx import masked_cls, masked_bbox, masked_landmark, combine_cls_bbox_landmark
 S=48
-if os.path.exists('cache{}.ims'.format(S)) and os.path.exists('cache{}.labels'.format(S)):
-    with open('cache{}.ims'.format(S), 'rb') as f:
-        #ims_all = pickle.load(f)
-        ims_all = msgpack.unpack(f, default=m.encode)
-    with open('cache{}.labels'.format(S), 'rb') as f:
-        #labels_all = pickle.load(f)
-        labels_all = msgpack.unpack(f, default=m.encode)
+if os.path.exists('cache{}.h5'.format(S)):
+    h5 = tables.open_file('cache48.h5', mode='r')
+    ims_all = h5.root.ims.read()
+    labels_all = h5.root.labels.read()
+    h5.close()
+    
 else:
     with open(r'../48net/48/pts.imdb', 'rb') as fid:
         pts = pickle.load(fid)
@@ -61,19 +61,20 @@ else:
     gc.collect()
 
     ims_all,labels_all = combine_cls_bbox_landmark(ims_cls, one_hot_labels, ims_roi, roi_score, ims_pts, pts_score)
-    with open('cache{}.ims'.format(S), 'wb') as f:
-        #pickle.dump(ims_all, f, protocol=0)
-        msgpack.pack(ims_all, f, default=m.encode)
-    with open('cache{}.labels'.format(S), 'wb') as f:
-        #pickle.dump(labels_all, f, protocol=0)
-        msgpack.pack(labels_all, f, default=m.encode)
+    del ims_cls, one_hot_labels, ims_roi, roi_score, ims_pts, pts_score
+
+    h5 = tables.open_file('cache48.h5', mode='w', title='All')
+    h5.create_array(h5.root, 'ims', ims_all)
+    h5.create_array(h5.root, 'labels', labels_all)
+    h5.close()
+
 
 from MTCNNx import create_Kao_Onet
 model = create_Kao_Onet(r'model48.h5')
 
-lr = 0.001
+lr = 0.01
 batch_size = 1024*10
-for i_train in range(2):
+for i_train in range(10):
     print('round ', i_train)
     lr = lr * 0.5
     my_adam = adam(lr = lr)
@@ -93,8 +94,10 @@ for i_train in range(2):
         'landmark':masked_landmark
     }
 
-    parallel_model = keras.utils.multi_gpu_model(model, gpus=2)
-    parallel_model.compile(loss=loss_list, optimizer = my_adam, loss_weights=loss_weights_list, metrics=metrics_list)
-    parallel_model.fit([ims_all], [labels_all, labels_all, labels_all], batch_size=batch_size, epochs=1)
+    #parallel_model = keras.utils.multi_gpu_model(model, gpus=2)
+    #parallel_model.compile(loss=loss_list, optimizer = my_adam, loss_weights=loss_weights_list, metrics=metrics_list)
+    #parallel_model.fit([ims_all], [labels_all, labels_all, labels_all], batch_size=batch_size, epochs=1)
+    model.compile(loss=loss_list, optimizer = my_adam, loss_weights=loss_weights_list, metrics=metrics_list)
+    model.fit([ims_all], [labels_all, labels_all, labels_all], batch_size=batch_size, epochs=2)
     model.save_weights('model48.h5')
     print('model saved')
